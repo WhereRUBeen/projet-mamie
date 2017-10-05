4d3ba264ff7cd68d5114f73e36651fa7e2e59ad9
Unknown <brice.mimifir.a@gmail.com>
2017-10-05 08:53:22 -0400

    New class from packtage entities
    Add class Commande
    Add class Contrat
    Add class Historique
    Add class ingredient
    Add class Magasin
    Add class Produit
    Add class Recette
    Add class Recette_ingredient
    Add class Vente
    Add class Utilisateur

---
+++ src/entities/Utilisateur.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package entities;
+
+import java.io.Serializable;
+
+/**
+ * @author Been WhereU
+ *
+ */
+@SuppressWarnings("serial")
+public class Utilisateur implements Serializable{
+
+}
