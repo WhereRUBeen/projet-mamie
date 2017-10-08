a7df93a1e8ee066848860802dd10b7ef111c8a07
Unknown <ligno19@gmail.com>
2017-10-08 16:27:42 -0400

    1st js

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function logError() {
+    
+    let perror = document.getElementById('logError');
+    let merror = "Erreur, vos identifiants sont invalide";
+    
+    perror.appendChild(merror);
+    
+}
