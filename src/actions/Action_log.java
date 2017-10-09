61002aa6aa3762ccf85e97cfe40cb048f043e5e4
Unknown <brice.mimifir.a@gmail.com>
2017-10-09 15:11:59 -0400

    action log

--- src/actions/Action_log.java
+++ src/actions/Action_log.java
@@ -3,10 +3,30 @@
  */
 package actions;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import entities.Log;
+
+
 /**
  * @author Been WhereU
  *
  */
 public class Action_log {
+	
+	
+	public static void showLogById(HttpServletRequest request, int id_log) {
+		ArrayList<Log> magasinsId = Manager_utilisateur.getMagasinId(id_log);
+		request.setAttribute("magasinsId", magasinsId);
+	}
+	
+	public static void showLogByDate(HttpServletRequest request, Date date ){
+		ArrayList<Log> magasinsName = Manager_utilisateur.getMagasinName(date);
+		request.setAttribute("magasinsName", magasinsName);
+	} 
+
 
 }
