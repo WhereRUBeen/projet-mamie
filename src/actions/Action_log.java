c9a6794f38c5111d9aceacf6d759c551b160fb1f
Unknown <brice.mimifir.a@gmail.com>
2017-10-09 15:22:20 -0400

    Add method
    New method on Action_log

--- src/actions/Action_log.java
+++ src/actions/Action_log.java
@@ -2,14 +2,11 @@
  * 
  */
 package actions;
-
 import java.sql.Date;
 import java.util.ArrayList;
-
 import javax.servlet.http.HttpServletRequest;
-
 import entities.Log;
-
+import managers.Manager_log;
 
 /**
  * @author Been WhereU
@@ -19,13 +16,13 @@ public class Action_log {
 	
 	
 	public static void showLogById(HttpServletRequest request, int id_log) {
-		ArrayList<Log> magasinsId = Manager_utilisateur.getMagasinId(id_log);
-		request.setAttribute("magasinsId", magasinsId);
+		ArrayList<Log> LogId = Manager_log.getLogId(id_log);
+		request.setAttribute("LogId", LogId);
 	}
 	
 	public static void showLogByDate(HttpServletRequest request, Date date ){
-		ArrayList<Log> magasinsName = Manager_utilisateur.getMagasinName(date);
-		request.setAttribute("magasinsName", magasinsName);
+		ArrayList<Log> LogDate = Manager_log.getLogByDate(date);
+		request.setAttribute("LogDate", LogDate);
 	} 
 
 
