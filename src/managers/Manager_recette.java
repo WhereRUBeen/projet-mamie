0e7f9271d8df4cee8185aed7d3e7ccf3ccb2d918
Unknown <brice.mimifir.a@gmail.com>
2017-10-10 12:18:00 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/managers/Manager_recette.java
+++ src/managers/Manager_recette.java
@@ -3,10 +3,37 @@
  */
 package managers;
 
+import java.util.ArrayList;
+
+import actions.HttpServletRequest;
+import entities.Ingredient;
+
 /**
  * @author Been WhereU
  *
  */
 public class Manager_recette {
+	
+	
+	public static void showIngredientById(HttpServletRequest request , int id_ingredient) {
+		ArrayList<Ingredient> ingredientId = Manager_ingredient.getIngredientById(id_ingredient);
+		request.setAttribute("ingredientId", ingredientId);
+	}
+	
+	public static void ShowingredientByName(HttpServletRequest request, String nom_ingredient) {
+		ArrayList<Ingredient> IngredientName = Manager_utilisateur.getIngredientByname(nom_ingredient);
+		request.setAttribute("IngredientName", IngredientName);
+	}
+	
+	
+	public static void deletedIngredientById(HttpServletRequest request, String prenom) {
+		ArrayList<Ingredient> ingredientId = Manager_ingredient.getByprenom(prenom);
+		request.setAttribute("utilisateurByname", utilisateurlastName);
+	}
+	
+	public static void addIngredientById(HttpServletRequest request,int id_ingredient, Ingredient ingredientToAdd){
+		Manager_utilisateur.add(ingredientToAdd);
+		showIngredientById(request, id_ingredient);
+	}
 
 }
