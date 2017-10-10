42717742c9e2bf12572c5f1c26b8c55a3e2a80b9
Unknown <brice.mimifir.a@gmail.com>
2017-10-10 13:30:20 -0400

    maj

--- src/managers/Manager_produit.java
+++ src/managers/Manager_produit.java
@@ -14,6 +14,7 @@ import entities.Produit;
  *
  */
 public class Manager_produit {
+	private static Sting afficherProduit = "select * from produit";
 	private static String afficherProduitParId = "";
 	private static String afficherProduitParNom = "";
 	private static String ajouterProduitParId = "";
